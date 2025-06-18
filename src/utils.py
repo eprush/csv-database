000fabafbd15811349f3ef866d5997a9e52a8a14
Egor Pavlovich <pavlovich.er@phystech.edu>
2025-06-19 01:24:38 +0300

    add get_headers and parse_row

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,21 @@
-from collections.abc import Iterable
+from collections.abc import Iterable, Sequence
 
 
 def has_csv_extension(filename: str) -> bool:
     return len(filename) < 4 or filename[-4:] == ".csv"
 
 
-def does_column_exist(reader: Iterable, column_name: str) -> bool:
-    headers = next(iter(reader))
+def parse_row(row: Sequence[str], *, is_headers: bool = False) -> list[str]:
+    result = row[0].strip().split(";")
+    if is_headers:
+        result[0] = result[0][3:]
+    return result
+
+
+def get_headers(reader: Iterable[Sequence[str]]) -> list[str]:
+    headers_row = next(iter(reader))
+    return parse_row(headers_row, is_headers=True)
+
+
+def does_column_exist(column_name: str, *, headers: Iterable[str]) -> bool:
     return column_name in headers
