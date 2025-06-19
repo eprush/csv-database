8a12d3af1cdb25a938f75ae62ca4df8111574100
Egor Pavlovich <pavlovich.er@phystech.edu>
2025-06-19 20:24:42 +0300

    add test_digital_column and test_parse_row

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,28 +1,35 @@
-from collections.abc import Iterable
+from collections.abc import Iterable, Sequence
+from contextlib import nullcontext as does_not_raise
+from typing import Literal
+
 import pytest
 
-from src.utils import does_column_exist, has_csv_extension
+from src.utils import does_column_exist, has_csv_extension, is_digital_column, parse_row
 
-class CustomReader:
+
+class CustomHeaders:
     def __init__(self):
-        self._values = (["brand", "cost", "reviews", "publication year"], ["", "", "", ""])
+        self._values = ["brand", "cost"]
 
     def __iter__(self):
         return iter(self._values)
 
+
 @pytest.fixture
-def reader() -> Iterable:
-    return CustomReader()
+def headers() -> Iterable:
+    return CustomHeaders()
+
 
 @pytest.mark.parametrize(("column", "res"),
     [
         ("brand", True),
-        ("reviews", True),
+        ("cost", True),
         ("rating", False),
     ]
 )
-def test_does_exist_column(reader, column, res):
-    assert does_column_exist(reader, column) == res
+def test_does_exist_column(headers, column, res):
+    assert does_column_exist(column, headers=headers) == res
+
 
 @pytest.mark.parametrize(("filename", "res"),
     [
@@ -34,3 +41,28 @@ def test_does_exist_column(reader, column, res):
 )
 def test_has_csv_extension(filename, res):
     assert has_csv_extension("tests/data/" + filename) == res
+
+
+@pytest.mark.parametrize(("values", "res", "expectation"),
+    [
+        ([1,2,3], True, does_not_raise()),
+        ([0,"-0.1"], True, does_not_raise()),
+        ([1, "1", 3], True, does_not_raise()),
+        (["0,1"], False, does_not_raise()),
+        (["test", 1, 1, 1], False, does_not_raise()),
+        ([], ..., pytest.raises(ValueError)),
+    ]
+)
+def test_is_digital_column(values: Sequence, res, expectation):
+    with expectation:
+        assert is_digital_column(values) == res
+
+
+@pytest.mark.parametrize(("row", "sep", "res"),
+    [
+        (["1.0;1;test;1,0"], ";", ["1.0", "1", "test", "1,0"]),
+        (["1.0,1,test,1;0"], ",", ["1.0", "1", "test", "1;0"]),
+    ]
+)
+def test_parse_row(row, sep: Literal[";", ","], res):
+    assert parse_row(row, sep=sep) == res
